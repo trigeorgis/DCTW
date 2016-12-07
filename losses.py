23d588503d9eb554ab194358157cafe3086e285c
George Trigeorgis <gt108@ic.ac.uk>
2016-12-07 13:07:50 +0000

    removed source covariance as a ret val

--- losses.py
+++ losses.py
@@ -28,7 +28,8 @@ def _trace_norm_grad(op, grad, g_u, g_v):
 def regularize(inputs, regularisation):
     return inputs + tf.ones_like(inputs) * regularisation
 
-def correlation_cost(source, target, source_regularisation=1e-6, target_regularisation=1e-6):
+
+def correlation_cost(source, target, source_regularisation=0, target_regularisation=0, use_target=True):
     num_source_samples = source.get_shape().as_list()[0]
     num_target_samples = target.get_shape().as_list()[0]
     
@@ -37,26 +38,26 @@ def correlation_cost(source, target, source_regularisation=1e-6, target_regulari
     num_samples = tf.to_float(tf.shape(target)[0])
     
     source -= tf.reduce_mean(source, 0)
+    
     target -= tf.reduce_mean(target, 0)
     
     correlation_matrix = tf.matmul(source, target, transpose_a=True) / (num_samples - 1)
 
     source_covariance = regularize(tf.matmul(source, source, transpose_a=True) / (num_samples-1), source_regularisation) 
-    target_covariance = regularize(tf.matmul(target, target, transpose_a=True) / (num_samples-1), source_regularisation)
     
     # source_covariance = (tf.transpose(source_covariance) + source_covariance) / 2.
     
     root_source_covariance = tf.cholesky(source_covariance)
     inv_root_source_covariance = tf.matrix_inverse(root_source_covariance)
 
-    # target_covariance = (tf.transpose(target_covariance) + target_covariance) / 2.
-    
-    root_target_covariance = tf.cholesky(target_covariance)
-    inv_root_target_covariance = tf.matrix_inverse(root_target_covariance)
-
     canonical_correlation = tf.matmul(inv_root_source_covariance, correlation_matrix)
-    canonical_correlation = tf.matmul(canonical_correlation, inv_root_target_covariance)
     
-    loss = _trace_norm.trace_norm(canonical_correlation)[0]
+    if use_target:
+        target_covariance = regularize(tf.matmul(target, target, transpose_a=True) / (num_samples-1), source_regularisation)
+        root_target_covariance = tf.cholesky(target_covariance)
+        inv_root_target_covariance = tf.matrix_inverse(root_target_covariance)
+        canonical_correlation = tf.matmul(canonical_correlation, inv_root_target_covariance)
+    
+    loss, u, v = _trace_norm.trace_norm(canonical_correlation)
 
-    return - loss, source_covariance
+    return - loss
